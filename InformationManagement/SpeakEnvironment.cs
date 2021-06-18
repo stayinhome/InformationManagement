7a91f186eee1d137516a1d2a12816a7695f0db55
远晖 罗 <871844240@qq.com>
2021-06-18 15:48:09 +0800

    新增

---
+++ InformationManagement/SpeakEnvironment.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace InformationManagement
+{
+    public class SpeakEnvironment
+    {
+        /// <summary>
+        /// 士气值
+        /// </summary>
+        public int Morale = 0;
+
+        /// <summary>
+        /// 氛围
+        /// </summary>
+        public AtmosphereType AtmosphereType = AtmosphereType.None;
+
+    }
+
+
+    public enum AtmosphereType
+    {
+        None = 0,
+    }
+}
